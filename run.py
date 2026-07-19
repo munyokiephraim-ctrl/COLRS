2a5207b5c16c791cc9c1915e846d1eb3de352c04
ephu-kimanzi <ephukimanzi1469@gmail.com>
2026-07-19 17:30:44 +0300

    Manually register blueprints

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from app import create_app, db
+from app.auth.routes import auth_bp
+from app.student.routes import student_bp
+from app.admin.routes import admin_bp
 
 app = create_app()
+app = Flask(__name__)
+
+# Register them here
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(student_bp, url_prefix='/student')
+app.register_blueprint(admin_bp, url_prefix='/admin')
 
 # This is the magic block that forces table creation
 with app.app_context():
